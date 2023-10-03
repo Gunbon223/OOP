162662a6648db694ba34580c40cef2c8c47fd882
Gunbon223 <gunbon610@gmail.com>
2023-10-03 09:57:21 +0700

    bvn2

--- BTex/src/Main.java
+++ BTex/src/Main.java
@@ -1,21 +1,18 @@
-import java.util.Scanner;
 
 public class Main {
-    public static double doiDoFtoC(double F) {
-        return (F-32)/1.8888888;
-    }
+
     public static void main(String[] args) {
+        String s=  "I love you";
+        s = s + " forever";
+        System.out.println(s);
+        System.out.println(s.substring(0,8));
+        String[] word= s.split(" ");
+        String s1 = word[0];
+        String s2 = word[1];
+        String s3 = word[2];
+        s2 = "hate";
+        System.out.println(s1+" "+s2+" "+s3);
 
-        // Cau 1 in ra 1.0 vi 2 so kieu int nen se in ra phan nguyen thoi
-        double  d = 9.0 /5;
-                System.out.println(d);
-
-        //cau2
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap do F de doi: ");
-        double a = sc.nextDouble();
-
-        System.out.println(doiDoFtoC(a) +" C");
 
     }
 
